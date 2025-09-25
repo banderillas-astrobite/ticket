1ff994c5ba40d3f26ce27ce71eb0d223425c6a3d
Isaac Ocampo <banderillas.astro.bite@gmail.com>
2025-09-24 23:03:23 -0600

    Actualizar build para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/ticket/',  // Esto es muy importante para GitHub Pages
   plugins: [react()],
-  base: "./", // <- rutas relativas para GH Pages
 });
