591945458fdf1fb68f0a87d32bb59cdde02d0f07
Isaac Ocampo <banderillas.astro.bite@gmail.com>
2025-09-24 22:11:40 -0600

    Deploy Vite React app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ticket/", // <-- importante para GitHub Pages
   plugins: [react()],
-  build: {
-    outDir: "docs",
+  base: '/ticket/', // <- IMPORTANTE para que GitHub Pages encuentre los archivos correctamente
+  server: {
+    port: 5173, // puedes cambiar el puerto si quieres
+    open: true, // abre el navegador automáticamente al correr npm run dev
   },
-});
+  build: {
+    outDir: 'dist', // carpeta de salida para producción
+    sourcemap: true, // opcional: ayuda a depurar en producción
+  },
+})
