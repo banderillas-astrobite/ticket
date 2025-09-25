3a8f9c79462d16ec2f09e116554e25d9adb7445f
Isaac Ocampo <banderillas.astro.bite@gmail.com>
2025-09-24 22:53:28 -0600

    Deploy frontend con rutas relativas corregidas

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ticket/', // <- IMPORTANTE para que GitHub Pages encuentre los archivos correctamente
-  server: {
-    port: 5173, // puedes cambiar el puerto si quieres
-    open: true, // abre el navegador automáticamente al correr npm run dev
-  },
-  build: {
-    outDir: 'dist', // carpeta de salida para producción
-    sourcemap: true, // opcional: ayuda a depurar en producción
-  },
-})
+  base: "./", // <- rutas relativas para GH Pages
+});
